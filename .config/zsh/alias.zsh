b10fa2cc26d753f134d58b43ec42c8acec79ea3a
Hendrik <hendrik.s.debruin@gmail.com>
2020-07-29 17:44:42 +0200

    feat: added glog alias...
    opens `git log` inside of vim with nice syntax highlighting

--- .config/zsh/alias.zsh
+++ .config/zsh/alias.zsh
@@ -40,6 +40,7 @@ alias cmatlab="rlwrap -a matlab -nodesktop -nosplash"
 alias top="htop"
 alias ast="clang -Xclang -ast-dump -fsyntax-only"
 alias gitgrs="git log --graph --oneline --branches --color | head -n 50"
+alias glog="git log | vim -"
 
 # ==============================================================================
 # Convenience
