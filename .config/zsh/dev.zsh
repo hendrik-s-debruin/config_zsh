6598dba60bc28180eaed13665565bfcc57cda044
Hendrik <hendrik.s.debruin@gmail.com>
2020-09-22 15:26:07 +0200

    feat: add conan settings

--- .config/zsh/dev.zsh
+++ .config/zsh/dev.zsh
@@ -1,5 +1,6 @@
 # This file sets variables for the development environment of various languages
 
 RUST_BACKTRACE=1
-CMAKE_EXPORT_COMPILE_COMMANDS=ON
+export CMAKE_EXPORT_COMPILE_COMMANDS=ON
+CONAN_ERROR_ON_OVERRIDE=True
 source ~/programming/sdk/emscripten/emsdk/emsdk_env.sh > /dev/null
