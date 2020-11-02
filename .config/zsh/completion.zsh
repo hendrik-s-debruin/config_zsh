9bff5a5f9a55220ca4e6220396c5f11f824f167c
Hendrik de Bruin <hendrik.s.debruin@gmail.com>
2020-11-02 13:33:31 +0100

    feat: fzf history search

--- .config/zsh/completion.zsh
+++ .config/zsh/completion.zsh
@@ -6,6 +6,16 @@ source ~/.config/zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
 # Fzf completions
 [ -f /usr/share/fzf/completion.zsh ] && source /usr/share/fzf/completion.zsh
 
+# Search history
+search_history() {
+	BUFFER=$(history -t '%Y-%m-%d %H:%M:%S' 0 | grep -v 1969 | fzf +s +m -x --tac -q "$BUFFER" | awk '{print substr($0, index($0, $4))}')
+
+	zle end-of-line
+}
+autoload search_history
+zle -N search_history
+bindkey '^r' search_history
+
 autoload -Uz compinit
 compinit
 
