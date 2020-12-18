e076b063017fde7bada9d179f1d6714d61763b3f
Hendrik de Bruin <hendrik.s.debruin@gmail.com>
2020-12-18 11:46:18 +0100

    feat: add cargo installs to PATH

--- .config/zsh/dev.zsh
+++ .config/zsh/dev.zsh
@@ -1,6 +1,8 @@
 # This file sets variables for the development environment of various languages
 
 RUST_BACKTRACE=1
+export PATH=$PATH:~/.cargo/bin
+
 export CMAKE_EXPORT_COMPILE_COMMANDS=ON
 CONAN_ERROR_ON_OVERRIDE=True
 source ~/programming/sdk/emscripten/emsdk/emsdk_env.sh > /dev/null
