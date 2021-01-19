cb28ed0ee97d6a4e8f9de3c265de0011b632b749
Hendrik de Bruin <hendrik.s.debruin@gmail.com>
2021-01-19 15:16:25 +0100

    feat: source ros

--- .config/zsh/dev.zsh
+++ .config/zsh/dev.zsh
@@ -5,4 +5,6 @@ export PATH=$PATH:~/.cargo/bin
 
 export CMAKE_EXPORT_COMPILE_COMMANDS=ON
 CONAN_ERROR_ON_OVERRIDE=True
-source ~/programming/sdk/emscripten/emsdk/emsdk_env.sh > /dev/null
+source ~/programming/sdk/emscripten/emsdk/emsdk_env.sh 2> /dev/null
+
+source /opt/ros/noetic/setup.zsh
