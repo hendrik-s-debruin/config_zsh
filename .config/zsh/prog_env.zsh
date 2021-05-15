61f72bb16e118e5d1d5ed8b7322a692758943345
Hendrik de Bruin <hendrik.s.debruin@gmail.com>
2021-05-15 17:39:30 +0200

    feat: rclone uses pass

--- .config/zsh/prog_env.zsh
+++ .config/zsh/prog_env.zsh
@@ -5,5 +5,8 @@ RANGER_LOAD_DEFAULT_RC=false
 GPG_TTY=$(tty)
 export GPG_TTY
 
+# Remote storage
+export RCLONE_PASSWORD_COMMAND="pass rclone/config"
+
 # how long pass should keep a password on the clipboard, seconds
 export PASSWORD_STORE_CLIP_TIME=10
