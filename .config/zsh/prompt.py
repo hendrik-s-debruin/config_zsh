833867e3016cc6aa29840ff47d9d16f1c4c2d09a
Hendrik de Bruin <hendrik.s.debruin@gmail.com>
2022-04-21 19:28:28 +0200

    feat: prompt changes length depending on available terminal space
    Some parts of the original prompt have not been ported to the new Python
    programme handling the prompt generation. These are commented out
    sections in the original file.

---
+++ .config/zsh/prompt.py
@@ -0,0 +1,196 @@
+#!/usr/bin/env python3
+
+#  ================================== Imports ============================== {{{
+from __future__ import annotations
+import os
+import git
+import sys
+from typing import List
+# }}}
+
+#  ================================== Colors =============================== {{{
+black = "\033[0;30m"
+red = "\033[0;31m"
+green = "\033[0;32m"
+yellow = "\033[0;33m"
+blue = "\033[0;34m"
+magenta = "\033[0;35m"
+cyan = "\033[0;36m"
+white = "\033[0;37m"
+bright_black = "\033[0;90m"
+bright_red = "\033[0;91m"
+bright_green = "\033[0;92m"
+bright_yellow = "\033[0;93m"
+bright_blue = "\033[0;94m"
+bright_magenta = "\033[0;95m"
+bright_cyan = "\033[0;96m"
+bright_white = "\033[0;97m"
+
+reset = "\033[0m"
+# }}}
+
+#  ================================ PromptEntry ============================ {{{
+class PromptEntry:
+    def __init__(self, entry: str, color: str = bright_white):
+        self.entry: str = color + entry + reset
+        self.len: int = len(entry)
+
+def prompt_cwd() -> PromptEntry:
+    """
+    Return the top two directories of the current path.
+    """
+    cwd = os.getcwd()
+    home = os.path.expanduser("~")
+
+    if cwd.startswith(home):
+        cwd = cwd.replace(home, "~", 1)
+
+    # Find the top two directories and store them in the tail variables
+    (head, tail) = os.path.split(cwd)
+    (head2, tail2) = os.path.split(head)
+
+    if head2 == "/":
+        # If we are close to root, display that
+        cwd = os.path.join(head2, tail2, tail)
+    else:
+        # Else, just display the top two directories
+        cwd = os.path.join(tail2, tail)
+
+    return PromptEntry(" " + cwd, yellow)
+
+
+def prompt_char() -> PromptEntry:
+    if get_success():
+        color = bright_green
+    else:
+        color = bright_red
+    return PromptEntry("λ", color)
+
+
+# TODO do not duplicate code with prompt_git_icon
+def prompt_git() -> PromptEntry:
+    try:
+        repo = git.Repo(os.getcwd())
+        branch_name = repo.active_branch.name
+        if repo.is_dirty():
+            color = bright_red
+        else:
+            color = green
+        return PromptEntry(branch_name, color)
+
+    except git.InvalidGitRepositoryError:
+        return PromptEntry("")
+
+
+# TODO do not duplicate code with prompt_git
+def prompt_git_icon() -> PromptEntry:
+    try:
+        repo = git.Repo(os.getcwd())
+        if repo.is_dirty():
+            color = bright_red
+        else:
+            color = green
+        return PromptEntry("", color)
+
+    except git.InvalidGitRepositoryError:
+        return PromptEntry("")
+
+
+def prompt_user() -> PromptEntry:
+    if get_success():
+        return PromptEntry(os.getlogin())
+    else:
+        return PromptEntry(os.getlogin(), red)
+
+
+def prompt_host() -> PromptEntry:
+    if get_success():
+        return PromptEntry("@ " + os.uname()[1])
+    else:
+        return PromptEntry("@ " + os.uname()[1], red)
+
+# }}}
+
+#  ================================== Layout =============================== {{{
+class PromptLayoutEngine:
+    class PromptSection:
+        def __init__(self, entry: PromptEntry, position: int, priority: int):
+            self.entry = entry
+            self.position = position
+            self.priority = priority
+            self.active = True
+
+    def __init__(self):
+        self.elements: List[PromptLayoutEngine.PromptSection] = []
+
+        self._add_element(prompt_user(), 2)
+        self._add_element(prompt_host(), 5)
+        self._add_element(prompt_cwd(), 3)
+        self._add_element(prompt_git_icon(), 1)
+        self._add_element(prompt_git(), 4)
+        self._add_element(prompt_char(), 0)
+
+        self.elements = list(filter(lambda x: x.entry.len > 0, self.elements))
+
+    def _add_element(self, element: PromptEntry, priority: int):
+        self.elements.append(
+            PromptLayoutEngine.PromptSection(element, len(self.elements), priority)
+        )
+
+    def build_prompt(self, available_cols: int):
+        self.elements = sorted(self.elements, key=lambda x: x.priority)
+
+        # NOTE this is a dangerous loop, but I'm too lazy right now to check for
+        # infinite loop conditions
+        while self._find_prompt_len(self.elements) > available_cols:
+            self.elements.pop()
+
+        # TODO add back elements
+
+        self.elements = sorted(self.elements, key=lambda x: x.position)
+        prompt: str = ""
+        for element in self.elements:
+            prompt += element.entry.entry + " "
+
+        return prompt
+
+    def _find_prompt_len(
+        self, prompt_entries: List[PromptLayoutEngine.PromptSection]
+    ) -> int:
+        length = 0
+
+        for element in prompt_entries:
+            length += element.entry.len
+        length += len(prompt_entries)  # spaces between entries
+
+        return length
+# }}}
+
+#  ============================= Argument Passing ========================== {{{
+# TODO improve this hacky argument passing
+def get_cols() -> int:
+    try:
+        return int(sys.argv[1])
+    except:
+        return 100
+
+
+def get_success() -> bool:
+    try:
+        return int(sys.argv[2]) == 0
+    except:
+        return False
+# }}}
+
+#  ============================ Interactive Testing ======================== {{{
+def demo():
+    prompt_builder = PromptLayoutEngine()
+    for i in range(100, 0, -1):
+        print(prompt_builder.build_prompt(i))
+# }}}
+
+#  =================================== main ================================ {{{
+if __name__ == "__main__":
+    prompt_builder = PromptLayoutEngine()
+    print(prompt_builder.build_prompt(get_cols()))
+# }}}
--- .config/zsh/prompt.zsh
+++ .config/zsh/prompt.zsh
@@ -1,124 +1,40 @@
 # This file configures the prompt of the zsh shell
 
-autoload -U colors && colors
 setopt PROMPT_SUBST
-PS1='$(prompt_whoami) '
-PS1+='@ '
-PS1+='$(hostname) '
-PS1+='$(prompt_pwd)'
-PS1+=' $(prompt_git)'
-PS1+='$(prompt_python_venv)'
-PS1+='$(prompt_job_count)'
-PS1+='$(prompt_emoji)'
-PS1+=' $(prompt_char) '
 
-# ==============================================================================
-# Build Prompt
-# ==============================================================================
-# TODO do not hard code colours, read from Xresources
-precmd () {
-	exit_status=$?
-}
+PS1='$(python ~/.config/zsh/prompt.py $COLUMNS $?)'
 
-# ------------------------------------------------------------------------------
-# git
-# ------------------------------------------------------------------------------
-function prompt_git {
+# TODO port this
+# function prompt_job_count {
+# 	local cnt=$(jobs | wc -l)
+# 	if [[ $cnt != 0 ]]; then
+# 		# echo $(color_string "[$cnt]" ebcb8b)
+# 		echo "%{$fg[yellow]%}[$cnt]"
+# 	fi
+# }
 
-	# Git Stuff
-	local output=`git branch 2>/dev/null`
-	if [[ $output != "" ]]; then
-		local branch_name=`echo $output | grep '*' | awk '{ print $2}'`
-		if [[ `git status --porcelain` ]]; then
-			# There are changes on the branch
-			# echo `color_string "[$branch_name]" bf616a`
-			echo "%{$fg[yellow]%} $branch_name "
-		else
-			# echo `color_string "[$branch_name]" a3be8c`
-			echo "%{$fg[green]%} $branch_name "
-		fi
-	else
-		#echo not on branch
-	fi
-}
+# TODO port this
+# function prompt_emoji {
+# 	if [[ $exit_status != 0 ]]; then
+# 		smileys=(
+# 			$(cat ~/.config/zsh/prompt_on_error)
+# 		)
 
-# ------------------------------------------------------------------------------
-# pwd
-# ------------------------------------------------------------------------------
-function prompt_pwd {
-	if [[ "$(pwd)" == "/" || "$(pwd)" == "$HOME" ]]; then
-		echo "%{$fg[yellow]%}%2~"
-	else
-		# echo $(color_string "%2~" ebcb8b)
-		echo "%{$fg[yellow]%} %2~"
-	fi
-}
+# 		index=$(head -200 /dev/urandom | cksum | awk '{print $1}')
+# 		index=$((1 + $index % ${#smileys[@]}))
+# 		# echo $(color_string ${smileys[$index]} bf616a)" "
+# 		echo "%{$fg[red]%}${smileys[$index]}"
 
-# ------------------------------------------------------------------------------
-# whoami
-# ------------------------------------------------------------------------------
-function prompt_whoami {
-	if [[ $exit_status == 0 ]]; then
-		echo $(bold_string "$(whoami) ")
-	else
-		# echo $(bold_string $(color_string "$(whoami)" bf616a))" "
-		echo "%{$fg[red]%}$(whoami)"
-	fi
-}
+# 	fi
+# }
 
-# ------------------------------------------------------------------------------
-# jobcount
-# ------------------------------------------------------------------------------
-function prompt_job_count {
-	local cnt=$(jobs | wc -l)
-	if [[ $cnt != 0 ]]; then
-		# echo $(color_string "[$cnt]" ebcb8b)
-		echo "%{$fg[yellow]%}[$cnt]"
-	fi
-}
-
-# ------------------------------------------------------------------------------
-# prompt char
-# ------------------------------------------------------------------------------
-function prompt_char {
-	if [[ $exit_status == 0 ]]; then
-		# success
-		# echo $(bold_string $(color_string '> ' a3be8c))
-		echo "%{$fg[green]%}λ"
-	else
-		# failure
-		# echo $(bold_string $(color_string "[$exit_status]>" bf616a))
-		echo "%{$fg[red]%}[$exit_status]λ"
-	fi
-}
-
-# ------------------------------------------------------------------------------
-# Smiley Prompt
-# ------------------------------------------------------------------------------
-function prompt_emoji {
-	if [[ $exit_status != 0 ]]; then
-		smileys=(
-			$(cat ~/.config/zsh/prompt_on_error)
-		)
-
-		index=$(head -200 /dev/urandom | cksum | awk '{print $1}')
-		index=$((1 + $index % ${#smileys[@]}))
-		# echo $(color_string ${smileys[$index]} bf616a)" "
-		echo "%{$fg[red]%}${smileys[$index]}"
-
-	fi
-}
-function bold_string {
-	#echo -e "\033[1m$1\033[0m"
-	echo $1
-}
-
-# ------------------------------------------------------------------------------
-# Python Virtual Environment
-# ------------------------------------------------------------------------------
-function prompt_python_venv {
-	if [[ "$VIRTUAL_ENV" != "" ]]; then
-		venv_name=$(basename $VIRTUAL_ENV)
-		echo "%{$fg[white]%} $venv_name"
-	fi
-}
+# TODO port this
+# # ------------------------------------------------------------------------------
+# # Python Virtual Environment
+# # ------------------------------------------------------------------------------
+# function prompt_python_venv {
+# 	if [[ "$VIRTUAL_ENV" != "" ]]; then
+# 		venv_name=$(basename $VIRTUAL_ENV)
+# 		echo "%{$fg[white]%} $venv_name"
+# 	fi
+# }
